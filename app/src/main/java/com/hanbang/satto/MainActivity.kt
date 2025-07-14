51e8eadc59341626829cee42a0320e99427fb97d
jgeun <pjk8926@gmail.com>
2025-07-14 23:10:21 +0900

    [CHORE] Ktlint Action 테스트용 unused import 코드 제거

--- app/src/main/java/com/hanbang/satto/MainActivity.kt
+++ app/src/main/java/com/hanbang/satto/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hanbang.satto.ui.theme.SattoTheme
-import android.util.Log
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
