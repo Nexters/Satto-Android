b322c4a7dd52620d0024d5b2dcbfb443f434392b
JGeun <68798525+JGeun@users.noreply.github.com>
2025-07-15 11:38:18 +0900

    [FEAT] multi-module용 Gradle Plugin 생성 (#1)
    * [FEAT] multi-module용 Gradle Plugin 생성
    
    * [REFACTOR] Plugin ID 사용 대신 Plugin Class 직접 적용 방식으로 변경
    
    * [CHORE] Target SDK 35로 수정
    
    * [FIX] includeBuild 위치를 pluginManagement 블록 밖으로 이동하여 build-logic 정상 인식
    
    * [FIX] androidExtension 접근 전 com.android.library 플러그인 명시적으로 적용
    
    * [CHORE] 코루틴 버전 1.9.0-RC → 1.9.0 Stable로 변경

---
+++ build-logic/src/main/kotlin/com/hanbang/satto/SattoAndroidLibraryPlugin.kt
@@ -0,0 +1,22 @@
+package com.hanbang.satto
+
+import convention.configureComposeAndroid
+import convention.configureCoroutineAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ *
+ * @author   JGeun
+ * @created  2025/07/14
+ */
+class SattoAndroidLibraryPlugin : Plugin<Project> {
+	override fun apply(target: Project) {
+		with(target) {
+			pluginManager.apply("com.android.library")
+
+			configureComposeAndroid()
+			configureCoroutineAndroid()
+		}
+	}
+}
