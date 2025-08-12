56b36c7fbb2df000987e81b93f21d8a7df9135b9
Changyeop Lee <cy.lee@vinulabs.com>
2025-08-12 12:41:16 +0900

    [FEAT] 홈화면 UI 개발

---
+++ feature/home/src/main/java/com/hanbang/home/HomeViewModel.kt
@@ -0,0 +1,16 @@
+package com.hanbang.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+
+): ContainerHost<HomeUiState, HomeSideEffect>, ViewModel() {
+    override val container: Container<HomeUiState, HomeSideEffect> = container(HomeUiState(isLoading = true, content = HomeUiState.Content.empty))
+
+}
