eff239f9068bc58feda1324c765c3de953664dbd
jgeun <pjk8926@gmail.com>
2025-08-21 00:08:04 +0900

    [CHORE] version_up_1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 		applicationId = "com.hanbang.satto"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 1
-		versionName = "1.0"
+		versionCode = 2025_08_21_01
+		versionName = "1.0.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
